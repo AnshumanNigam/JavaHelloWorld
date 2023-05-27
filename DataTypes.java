e2884754a1500a19b66cc3e5295213d81a9b3231
Anshuman Nigam <119495979+AnshumanNigam@users.noreply.github.com>
2023-05-27 20:33:25 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,25 @@
+public class DataTypes
+{
+    public static void main(String args[])
+    {
+        byte a= 12;
+        short b= 327;
+        int c= 2147483647;
+        long d= 92230787457589485l;
+        float e= 3.03f;
+        double f= 1.88854;
+        char g= 'A';
+        boolean h= true;
+        String i="My name is Anshuman";
+        System.out.println("Byte---> "+a);
+        System.out.println("Short---> "+b);
+        System.out.println("Int---> "+c);        
+        System.out.println("Long---> "+d);
+        System.out.println("Float---> "+e);
+        System.out.println("Double---> "+f);
+        System.out.println("Char---> "+g);
+        System.out.println("Boolean---> "+h);
+        System.out.println("String---> "+i);
+    }
+}
+
